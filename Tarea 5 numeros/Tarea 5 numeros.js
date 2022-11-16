0b76561a1b915f255fd9b4afa876a8271c429b14
Alejandro Villamayor <avillamayor720@gmail.com>
2022-11-16 18:17:30 -0300

    tarea nro 5

---
+++ Tarea 5 numeros/Tarea 5 numeros.js
@@ -0,0 +1,24 @@
+// Crea un archivo JS que contenga las siguientes líneas
+
+// - Una variable que contenga tu altura en centímetros (entero)
+let altura_en_centimetros = 185; 
+
+// - Una variable que contenga tu altura en metros (número de coma flotante)
+
+let altura_en_metros= 1.85;
+
+// - Una variable que contenga tu peso en kilogramos (número de coma flotante)
+
+let peso_en_kilogramos = 104.2;
+
+// - Una variable que contenga tu altura en metros redondeada hacia arriba
+
+let altura_redondeada_metros = Math.ceil(altura_en_metros)
+console.log(altura_redondeada_metros)
+
+// - Una variable que contenga tu peso en kilogramos redondeado hacia abajo
+
+let peso_redondeado_en_kg = Math.floor(peso_en_kilogramos)
+console.log(peso_redondeado_en_kg)
+
+// - Una variable que contenga si "el máximo valor que se puede obtener en Javascript + 1" es igual al máximo valor que se puede obtener en Javascript
